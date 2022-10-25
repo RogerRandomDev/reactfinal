b8abb3810ad50f07d95a15c918808dce322331f1
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2022-10-25 15:23:28 -0700

    connecting to db now

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
-
 const connectDB =(uri)=>{
     return mongoose.connect(uri)
     .then(()=>console.log("Database Connection"))
     .catch((err)=>console.log(err))
 }
-module.exports = connectDB
+module.exports = {connectDB,process}
