0791a2ff5f47f602b713d2b2ada7556dae810b82
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2022-10-27 13:45:19 -0700

    user-model

--- backend/controllers/Business.js
+++ backend/controllers/Business.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 //returns the business database content
 const getBusiness = async (businessName) => {
-  var output = [];
+  var output = [null];
   try {
     await connectDB(process.env.MONGO_URI);
     await (output = await BusinessModel.find({ Name: businessName }));
@@ -12,11 +12,11 @@ const getBusiness = async (businessName) => {
   } catch (err) {
     console.log(err);
   }
-  return output;
+  return output[0];
 };
 //creates a business and adds it to the database
 const createBusiness = async (businessData) => {
-  if ((await getBusiness(businessData.Name).length) == 0) {
+  if ((await getBusiness(businessData.Name)) == null) {
     return { success: false, msg: 'Business already exists with name' };
   }
   try {
--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,1 +1,29 @@
 const { connectDB, process } = require('../db/connect');
+require('dotenv').config();
+//returns the user database content
+const getUser = async (userName) => {
+    var output = [null];
+    try {
+      await connectDB(process.env.MONGO_URI);
+      await (output = await UserModel.find({ Name: userName }));
+      console.log(output);
+    } catch (err) {
+      console.log(err);
+    }
+    return output[0];
+  };
+  //creates a user and adds it to the database
+  const createUser = async (userData) => {
+    if ((await getUser(userData.Name)) == null) {
+      return { success: false, msg: 'User already exists with name' };
+    }
+    try {
+      await connectDB(process.env.MONGO_URI);
+      UserModel.create([userData]);
+    } catch (err) {
+      console.log(err);
+    }
+    return { success: true, msg: 'User Created successfully' };
+  };
+  
+  module.exports = { getUser, createUser };
