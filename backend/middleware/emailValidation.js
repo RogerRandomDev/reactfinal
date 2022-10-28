6fefdaa670b993f09017cc685003f808457fda2f
Victor Malhotra <victorrmalhotra@gmail.com>
2022-10-28 14:38:19 -0700

    Make a emailValidation middleware

---
+++ backend/middleware/emailValidation.js
@@ -0,0 +1,12 @@
+var emailValidator = require("deep-email-validator");
+
+async function isEmailValid(email) {
+  return emailValidator.validate(email);
+}
+
+//create a server object:
+const emailValidation = async (email) => {
+  const { valid, reason, validators } = await isEmailValid(`${email}`);
+  return { valid, reason, validators };
+};
+module.exports = emailValidation;
