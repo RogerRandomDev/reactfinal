9f1d7eff7c185523dcd2c58ff6e298f956c88239
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2022-10-31 14:09:05 -0700

    added requests

---
+++ src/Utils/requests.js
@@ -0,0 +1,14 @@
+const url="http://localhost:5000/"
+
+const buildHeader=(request,content)=>{
+    Object.keys(content).forEach((key)=>request.setRequestHeader(key,content[key]))
+    return request
+}
+
+
+export const sendRequest=(path,type,contents)=>{
+    var xml=new XMLHttpRequest()
+    xml.open(type,url+path)
+    xml=buildHeader(xml,contents)
+    xml.send("")
+}
