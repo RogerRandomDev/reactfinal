4d9a809b42a8db111f52f818ee79d919b1211bad
B1gChungus <lruben095@west-mec.org>
2022-11-01 15:22:21 -0700

    nearly finished about page (company history, people info, terms/privacy links) and added router

---
+++ src/Pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+import AboutUs from "../Components/AboutPageAboutUs"
+import CompanyHistory from "../Components/AboutPageCompanyHistory"
+function AboutPage() {
+    return (
+        <>
+            <AboutUs />
+            <CompanyHistory />
+        </>
+    )
+}
+export default AboutPage
