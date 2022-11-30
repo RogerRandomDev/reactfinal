87b8b88560507349e1e954700966bb56bdf66772
Ragingfury6 <trentsuperman@gmail.com>
2022-11-30 13:59:28 -0700

    Time Works for chat

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-function Message({time, data,self,first}) {
+function Message({ time, data, self }) {
   return (
-    <div className={`w-max mb-4 ${self && "self-end"} ${first && "mt-auto"}`}>
-        <p className='text-right text-xs mb-1'>{time}</p>
-        <div className={`${self ? "bg-blue-400 text-[#eee]" : "bg-slate-300 text-[#111]"} px-4 py-2 rounded font-medium`}>{data}</div>
+    <div className={`w-max mb-4 ${self && 'self-end'} text-[#111]`}>
+      <p className='text-right text-xs mb-1'>{time}</p>
+      <div
+        className={`${
+          self ? 'bg-blue-400 text-[#eee]' : 'bg-slate-300 text-[#111]'
+        } px-4 py-2 rounded font-medium`}>
+        {data}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Message
+export default Message;
