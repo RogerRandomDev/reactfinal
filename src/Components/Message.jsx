e4bfd6d5cd8ea73f64d53acaed803afdcfc43039
Ragingfury6 <trentsuperman@gmail.com>
2022-11-30 14:55:02 -0700

    Chat Changes

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 function Message({ time, data, self }) {
   return (
     <div className={`w-max mb-4 ${self && 'self-end'} text-[#111]`}>
-      <p className='text-right text-xs mb-1'>{time}</p>
+      <p
+        className={`${
+          self ? 'text-right' : 'text-left'
+        } text-xs mb-1 text-slate-200`}>
+        {time}
+      </p>
       <div
         className={`${
           self ? 'bg-blue-400 text-[#eee]' : 'bg-slate-300 text-[#111]'
-        } px-4 py-2 rounded font-medium`}>
+        } px-4 py-2 rounded font-medium break-words max-w-[325px] sm:max-w-[400px] md:max-w-[500px]`}>
         {data}
       </div>
     </div>
--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect, useReducer } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Message from '../Components/Message';
 import socket from '../socket';
 import parseTime from '../Utils/parseTime';
 import { sendRequest } from '../Utils/requests';
 function Chat() {
+  let { chatID } = useParams();
   const [message, setMessage] = useState('');
   const [usersInConversation, setUsersInConversation] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
+  const [hasTokenInUrl, setHasTokenInUrl] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   const initialState = [];
   const messageReducer = (state, action) => {
@@ -41,27 +44,32 @@ function Chat() {
     /**
      * Get From Database
      */
+    setLoaded(false);
     const id = JSON.parse(localStorage.getItem('user'))._id;
     const token = localStorage.getItem('token');
-
-    sendRequest('chat/getMessages', 'POST', {
-      body: {
-        userToken: token,
-        targetID: window.location.href.split('?id=')[1],
-      },
-      query: {},
-    }).then((data) => {
-      let messages = JSON.parse(data).list;
-      messages = messages.map((m) => {
-        // need to remove all messages from database that dont have time
-        return {
-          type: m.sender === id ? 'self' : 'other',
-          data: m.message,
-          time: parseTime(m.time),
-        };
+    const tokenInUrl = chatID;
+    setHasTokenInUrl(tokenInUrl);
+    if (tokenInUrl) {
+      sendRequest('chat/getMessages', 'POST', {
+        body: {
+          userToken: token,
+          targetID: chatID,
+        },
+        query: {},
+      }).then((data) => {
+        let messages = JSON.parse(data).list;
+        messages = messages.map((m) => {
+          // need to remove all messages from database that dont have time
+          return {
+            type: m.sender === id ? 'self' : 'other',
+            data: m.message,
+            time: parseTime(m.time),
+          };
+        });
+        dispatch({ type: 'custom', payload: messages });
       });
-      dispatch({ type: 'custom', payload: messages });
-    });
+    }
+    // users no matter what
 
     sendRequest('chat/getMessages', 'POST', {
       body: {
@@ -78,6 +86,7 @@ function Chat() {
         },
       }).then((userInfo) => {
         setUsersInConversation(JSON.parse(userInfo));
+        setLoaded(true);
       });
     });
 
@@ -123,13 +132,13 @@ function Chat() {
 
   useEffect(() => {
     let cUser = usersInConversation.find((u) => {
-      return u._id === window.location.href.split('?id=')[1];
+      return u._id === chatID;
     });
     if (cUser) setCurrentUser(cUser);
   }, [usersInConversation]);
 
   const onMessage = (content) => {
-    const toUserID = window.location.href.split('?id=')[1];
+    const toUserID = chatID;
     // console.log('CLIENT USER ID -> ', toUserID);
     socket.emit('private message', {
       content,
@@ -152,21 +161,20 @@ function Chat() {
 
   return (
     <div className='flex h-[calc(100vh_-_56px)] flex-col sm:flex-row bg-[#404959] text-[#eee]'>
-      <div className='border-r border-slate-200 border-opacity-50 flex flex-col gap-4 w-full sm:w-[30%] p-4 max-w-md mr-1'>
+      <div className='sm:border-r border-slate-200 border-opacity-50 flex flex-col gap-4 w-full sm:w-[30%] p-4 max-w-md mr-1'>
         <div className='flex sm:flex-col flex-wrap gap-4'>
           {usersInConversation.map((user, idx) => {
-            // CHANGE TO LINK TAGS - NEED TO FIX BUG
             return (
               <a
                 key={idx}
-                href={`http://localhost:3000/chat?id=${user._id}`}
-                className='flex gap-4 items-center bg-blue-200 p-4 rounded cursor-pointer hover:bg-blue-800 group transition flex-shrink-0'>
+                href={`http://localhost:3000/chat/${user._id}`}
+                className='flex gap-4 items-center bg-[#6e799e] p-4 rounded cursor-pointer hover:bg-blue-800 group transition w-[45%] sm:w-auto'>
                 <img
                   src={`https://res.cloudinary.com/dztnsrrta/image/upload/${user.icon}`}
                   alt='Icon'
                   className='w-20 h-20 rounded-full object-cover'
                 />
-                <p className='text-xl text-[#111] group-hover:text-[#eee] transition'>
+                <p className='text-xl text-slate-200 group-hover:text-[#eee] transition'>
                   {user.username}
                 </p>
               </a>
@@ -175,54 +183,74 @@ function Chat() {
         </div>
       </div>
       {/* grid grid-rows-[auto_auto_1fr_auto]  max-h-[calc(100vh_-_56px)]*/}
-      <div className='m-6 grid grid-rows-[auto_auto_1fr_auto] max-h-[80%] w-full sm:w-[70%] max-w-7xl mx-auto'>
-        <div className='flex items-center gap-4 p-4'>
-          <img
-            src={`https://res.cloudinary.com/dztnsrrta/image/upload/${currentUser.icon}`}
-            alt='r'
-            className='w-20 h-20 rounded-full object-cover'
-          />
+      {hasTokenInUrl ? (
+        <div className='m-6 grid grid-rows-[auto_auto_1fr_auto] max-h-[80%] w-full sm:w-[70%] max-w-7xl mx-auto'>
+          <Link
+            to={`/profile?id=${currentUser._id}`}
+            className='flex items-center gap-4 p-4 group w-max'>
+            <img
+              src={`https://res.cloudinary.com/dztnsrrta/image/upload/${currentUser.icon}`}
+              alt='r'
+              className='w-20 h-20 rounded-full object-cover'
+            />
+            <div className=''>
+              <h3 className='font-semibold text-lg mb-2 group-hover:underline'>
+                {currentUser.username}
+              </h3>
+              <p>Active in the last day</p>
+            </div>
+          </Link>
+          <div className='h-px bg-slate-200 rounded w-[90%] mx-auto mb-4'></div>
+          <div className='bg-[#6e799e] flex flex-col-reverse p-4 overflow-auto'>
+            {state.map(({ type, data, time }, idx) => {
+              return (
+                <Message
+                  key={idx}
+                  time={time}
+                  data={data}
+                  self={type === 'self'}
+                />
+              );
+            })}
+          </div>
           <div className=''>
-            <h3 className='font-semibold text-lg mb-2'>
-              {currentUser.username}
-            </h3>
-            <p>Active in the last day</p>
+            <form
+              className='send-message w-full flex justify-between items-center border border-opacity-50 border-slate-200 sm:rounded-br sm:rounded-bl border-t-0 bg-[#404959]'
+              onSubmit={(e) => handleSubmit(e)}>
+              <input
+                type='text'
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                className='rounded px-4 py-8 w-full focus:outline-none max-w-full bg-[#404959]'
+                placeholder='Message...'
+                maxLength={190}
+                autoComplete='off'
+                name='Message Input'
+              />
+              <button
+                type='submit'
+                className='btn-primary w-max px-6 py-3 rounded bg-blue-500 hover:bg-blue-600 transition text-neutral-100 font-semibold mr-4'>
+                Send
+              </button>
+            </form>
           </div>
         </div>
-        <div className='h-px bg-slate-200 rounded w-[90%] mx-auto mb-4'></div>
-        <div className='bg-blue-100 flex flex-col-reverse p-4 overflow-auto'>
-          {state.map(({ type, data, time }, idx) => {
-            return (
-              <Message
-                key={idx}
-                time={time}
-                data={data}
-                self={type === 'self'}
-              />
-            );
-          })}
+      ) : (
+        <div className='grid place-items-center max-h-full w-full sm:w-[70%] max-w-7xl mx-auto'>
+          {usersInConversation.length === 0 && loaded ? (
+            <div>
+              <h2 className='text-center text-6xl mb-4'>
+                You Have No Open Chats
+              </h2>
+              <p className='text-center text-2xl'>
+                Navigate to a Product to Chat with a User
+              </p>
+            </div>
+          ) : (
+            <h2 className='text-center text-6xl'>Select a Chat to Begin</h2>
+          )}
         </div>
-        <div className=''>
-          <form
-            className='send-message w-full flex justify-between items-center border border-opacity-50 border-slate-200 rounded-br rounded-bl border-t-0'
-            onSubmit={(e) => handleSubmit(e)}>
-            <input
-              type='text'
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              className='rounded px-4 py-8 w-full focus:outline-none max-w-full bg-[#404959]'
-              placeholder='Message...'
-              autoComplete='off'
-              name='Message Input'
-            />
-            <button
-              type='submit'
-              className='btn-primary w-max px-6 py-3 rounded bg-blue-500 hover:bg-blue-600 transition text-neutral-100 font-semibold mr-4'>
-              Send
-            </button>
-          </form>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
